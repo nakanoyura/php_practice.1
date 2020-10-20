f8cb636cf785123f4f1f488bed803b7ae9d8613d
yura <2225ny@gmail.com>
2020-10-20 12:20:19 +0000

    課題追加

---
+++ practice.php
@@ -0,0 +1,38 @@
+<?php
+$name = "yura";
+
+if ($name == "yura") {
+    echo "私はyuraです。";
+} else {
+    echo "あなたの名前ではありません。";
+}
+
+
+$total = 0;
+for ($i = 0; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total;
+
+$fruits = array("apple","banana","orange","lemon","melon");
+
+foreach($fruits as $fruit){
+    echo $fruit;
+    echo "\n";
+    
+}
+
+
+/* for文の始めの値を定義する /
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
---
+++ sum.php
@@ -0,0 +1,50 @@
+<?php
+
+function sum($max){
+    $result = 0;
+    for($i = 1; $i <= $max; $i++){
+        $result += $i;
+    }
+    return $result;
+}
+echo sum(100);
+
+
+$str = "<h1>strip_tags関数</h1>"
+       ."<p>タグ取り除くよ！</p>";
+       echo strip_tags($str)."\n";
+       
+       $members=array("一郎","二郎","三郎");
+            array_push($members,"四郎","五郎");
+            print_r($members);
+            
+$array1 = [1, 2, 3];
+$array2 = [10, 20, 30];
+$array3 = [100, 200, 300];
+
+$array = array_merge($array1, $array2, $array3);
+
+print_r($array);
+
+
+
+echo '現在のUnixタイムスタンプ：'.time();
+ 
+$nweek = time() + (3 * 24 * 60 * 60);
+echo '3日後のUnixタイムスタンプ：'.$nweek;
+
+
+
+date_default_timezone_set('Asia/Tokyo');
+
+echo date("Y/m/d H:i:s") . "\n"; 
+echo date("Y/m/01") . "\n"; 
+echo date("Y/m/t") . "\n"; 
+
+$w = date("w");
+$week_name = array("日", "月", "火", "水", "木", "金", "土");
+
+echo date("Y/m/d") . "($week_name[$w])\n"; 
+
+?>
+
